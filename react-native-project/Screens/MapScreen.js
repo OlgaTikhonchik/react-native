598255a3718719a2c2d84be8453630f43424c5ad
OlgaTikhonchik <olga882011@gmail.com>
2023-06-27 19:56:19 +0300

    Create MapScreen.js

---
+++ react-native-project/Screens/MapScreen.js
@@ -0,0 +1,64 @@
+// import React from "react";
+// import { View, Text, StyleSheet } from "react-native";
+
+// export default function MapScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>MapScreen Screen</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+export default function MapScreen({ route }) {
+  const latitude = route.params?.latitude ?? 46.4775;
+  const longitude = route.params?.longitude ?? 30.7326;
+
+  return (
+    <View style={styles.main}>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.082,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        minZoomLevel={15}
+        onMapReady={() => console.log("Map is ready")}
+        onRegionChange={() => console.log("Region change")}
+      >
+        {/* {route.params && ( */}
+        <Marker
+          title={route.params?.name ?? "Моя локація"}
+          coordinate={{ latitude: latitude, longitude: longitude }}
+          description="hello"
+        />
+        {/* )} */}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    zIndex: 10,
+  },
+
+  mapStyle: {
+    width: "100%",
+    height: "100%",
+  },
+});
