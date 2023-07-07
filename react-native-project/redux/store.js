7bc4fbf3a2d80ee5936b26078212b93e6a3c288c
OlgaTikhonchik <olga882011@gmail.com>
2023-07-07 18:45:57 +0300

    Create store.js

---
+++ react-native-project/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice, authReducer } from "./auth/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    [authSlice.name]: authReducer,
+  },
+  devTools: true,
+});
