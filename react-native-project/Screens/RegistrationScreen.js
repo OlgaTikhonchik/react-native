901e4cd31a0b2997e29c040c229f89a1a00e1418
OlgaTikhonchik <olga882011@gmail.com>
2023-07-07 18:45:30 +0300

    Update RegistrationScreen.js

--- react-native-project/Screens/RegistrationScreen.js
+++ react-native-project/Screens/RegistrationScreen.js
@@ -1,280 +1,267 @@
-import BgImage from "../images/photoBg.jpg";
-import add from "../images/add.png";
 import {
-  Image,
-  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
   TextInput,
   TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-  StyleSheet,
+  Platform,
+  KeyboardAvoidingView,
   Keyboard,
-  Text,
-  ScrollView,
-  Alert,
+  TouchableWithoutFeedback,
+  Dimensions,
+  Image,
 } from "react-native";
-import { useEffect, useState } from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { authRegisterUser } from "../redux/auth/operations";
 import * as ImagePicker from "expo-image-picker";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function RegistrationScreen() {
-  const [login, setLogin] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [profilePhoto, setProfilePhoto] = useState("");
-  const [isButtonActive, setButtonActive] = useState(false);
-  const navigation = useNavigation();
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+  avatar: "",
+};
 
-  const onRegister = () => {
-    // if (!profilePhoto) {
-    //   Alert.alert("Please add profile photo");
-    //   return;
-    // }
-    Alert.alert(
-      "Credentials:",
-      `Login: ${login} 
-       Email: ${email} 
-       Password: ${password}`
-    );
-    // console.log("Credentials:", `${login} + ${email} + ${password}`);
-    if (login === "" || email === "" || password === "") {
-      Alert.alert("All fields must be filled");
-      // console.log("All fields must be filled");
-      return;
-    }
-    setLogin("");
-    setEmail("");
-    setPassword("");
-    navigation.navigate("Home");
-  };
+const RegistrationScreen = ({ navigation }) => {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [dimensions, setDimensions] = useState(
+    Dimensions.get("window").width - 16 * 2
+  );
 
-  const onShow = () => {
-    setShowPassword(!showPassword);
-    //Alert.alert("Credentials:", `Password: ${password}`);
-    // console.log("Show password:", `${password}`);
-  };
-
-  const showImagePicker = async () => {
-    const permissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (permissionResult.granted === false) {
-      return;
-    }
-    const result = await ImagePicker.launchImageLibraryAsync();
-    if (!result.canceled) {
-      setProfilePhoto(result.assets[0].uri);
-    }
-  };
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (login && email && password) {
-      setButtonActive(true);
-      return;
-    }
-    setButtonActive(false);
-  }, [login, email, password]);
+    const onChange = () => {
+      const width = Dimensions.get("window").width - 16 * 2;
+      setDimensions(width);
+    };
+    const subscription = Dimensions.addEventListener("change", onChange);
+    return () => subscription?.remove();
+  }, []);
 
-  const onRegisteredAccount = () => {
-    Alert.alert(
-      "Show registered account:",
-      `Login: ${login} 
-       Email: ${email} 
-       Password: ${password}`
-    );
-    // console.log("Show registered account");
+  const takeAvatar = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setState((prevstate) => ({
+        ...prevstate,
+        avatar: result.assets[0].uri,
+      }));
+    }
   };
+
+  const removeAvatar = async () => {
+    setState((prevstate) => ({
+      ...prevstate,
+      avatar: null,
+    }));
+  };
+
+  const handleSubmit = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log("state", state);
+    dispatch(authRegisterUser(state));
+    setState(initialState);
+  };
+
   return (
-    <>
-      <ScrollView style={{ width: "100%" }}>
-        <View style={styles.containerImage}>
-          <Image source={BgImage} style={styles.image} resizeMode="cover" />
-        </View>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : ""}
-          style={styles.container}
-          keyboardVerticalOffset={-240}
+    <TouchableWithoutFeedback onPress={handleSubmit}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../images/photoBg.jpg")}
+          style={styles.image}
         >
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.register}>
-              {/* <Image style={styles.imageAvatar} /> */}
-              <View style={styles.imageAvatar}>
-                {profilePhoto && (
-                  <Image
-                    source={{ uri: profilePhoto }}
-                    // style={styles.imageAvatar}
-                  />
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.wrapper,
+                paddingBottom: isShowKeyboard ? 32 : 78,
+
+                paddingHorizontal: 16,
+              }}
+            >
+              <View style={styles.wrapperText}>
+                <Text style={styles.text}>Регистрация</Text>
+              </View>
+              <View style={styles.avatar}>
+                {!state.avatar ? (
+                  <>
+                    <Image
+                      source={require("../images/userFoto.jpg")}
+                      style={{ width: 120, height: 120, borderRadius: 16 }}
+                    />
+
+                    <TouchableOpacity
+                      style={styles.avatarBtn}
+                      activeOpacity={0.7}
+                      accessibilityLabel="add avatar"
+                      onPress={takeAvatar}
+                    >
+                      <AntDesign name="pluscircleo" size={24} color="#FF6C00" />
+                    </TouchableOpacity>
+                  </>
+                ) : (
+                  <>
+                    <Image
+                      source={{ uri: state.avatar }}
+                      style={{ width: 120, height: 120, borderRadius: 16 }}
+                    />
+
+                    <TouchableOpacity
+                      style={styles.avatarBtn}
+                      activeOpacity={0.7}
+                      accessibilityLabel="delete avatar"
+                      onPress={removeAvatar}
+                    >
+                      <AntDesign
+                        name="closecircleo"
+                        size={24}
+                        color="#FF6C00"
+                      />
+                    </TouchableOpacity>
+                  </>
                 )}
               </View>
-              <TouchableOpacity onPress={showImagePicker}>
-                <Image source={add} style={styles.imageAdd} />
-              </TouchableOpacity>
-              <Text style={styles.text}>Реєстрація</Text>
               <TextInput
+                style={styles.input}
                 placeholder="Логін"
-                placeholderTextColor={"#BDBDBD"}
-                style={styles.input}
-                autoCompleteType="off"
-                value={login}
-                onChangeText={setLogin}
-              />
-              <TextInput
-                placeholder="Адреса електронної пошти"
-                placeholderTextColor={"#BDBDBD"}
-                style={styles.input}
-                autoCompleteType="off"
-                value={email}
-                onChangeText={setEmail}
-              />
-              <TextInput
-                placeholder="Пароль"
-                placeholderTextColor={"#BDBDBD"}
-                style={styles.input}
-                autoCompleteType="off"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={!showPassword}
-                // secureTextEntry
-              />
-              <TouchableOpacity style={styles.showTxt} onPress={onShow}>
-                <Text style={styles.showPasswordText}>
-                  {showPassword ? "Приховати" : "Показати"}
-                </Text>
-                {/* <Text style={styles.showPasswordText}>Показати</Text> */}
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={
-                  isButtonActive
-                    ? styles.registerBtn
-                    : styles.disabledRegisterBtn
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, userName: value }))
                 }
-                disabled={isButtonActive ? false : true}
-                // style={styles.registerBtn}
-                onPress={onRegister}
+                value={state.userName}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Адреса електронної пошти"
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, email: value }))
+                }
+                value={state.email}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Пароль"
+                secureTextEntry={true}
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, password: value }))
+                }
+                value={state.password}
+              />
+
+              <TouchableOpacity
+                style={styles.btn}
+                activeOpacity={0.7}
+                onPress={handleSubmit}
               >
+                <Text style={styles.textBtn}>Зареєстуватися</Text>
+              </TouchableOpacity>
+              <Text style={styles.textLogin}>
+                Вже є акаунт?{" "}
                 <Text
-                  style={
-                    isButtonActive
-                      ? styles.registerBtnText
-                      : styles.registerBtnTextDisabled
-                  }
+                  style={styles.textLogin}
+                  onPress={() => navigation.navigate("Login")}
                 >
-                  Зареєстуватися
+                  Увійти
                 </Text>
-              </TouchableOpacity>
-              {/* <TouchableOpacity onPress={onRegisteredAccount}> */}
-              <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                <Text style={styles.linkText}>Вже є акаунт? Увійти</Text>
-              </TouchableOpacity>
+              </Text>
             </View>
-          </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
-      </ScrollView>
-    </>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
+
+export default RegistrationScreen;
 
 const styles = StyleSheet.create({
-  containerImage: {
-    flex: 1,
-    position: "absolute",
-    height: "100%",
-    width: "100%",
-  },
   container: {
     flex: 1,
-    width: "100%",
+    backgroundColor: "#fff",
   },
   image: {
-    // flex: 1,
-    width: "100%",
+    flex: 1,
+    justifyContent: "flex-end",
   },
-  register: {
+  wrapper: {
     backgroundColor: "#fff",
-    width: "100%",
-    height: "100%",
-    borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    marginTop: 263,
+    borderTopStartRadius: 25,
   },
-  imageAvatar: {
-    position: "absolute",
-    backgroundColor: "#F6F6F6",
-    width: 120,
-    height: 120,
-    borderRadius: 16,
-    top: -60,
-    right: 150,
+  wrapperText: {
+    alignItems: "center",
   },
-  imageAdd: {
-    position: "absolute",
-    width: 25,
-    height: 25,
-    left: 250,
-    top: 21,
-  },
+
   text: {
+    color: "#212121",
     fontFamily: "Roboto-Medium",
     fontSize: 30,
-    textAlign: "center",
-    marginTop: 92,
-    color: "#212121",
-    fontWeight: 500,
+    lineHeight: 35,
+    letterSpacing: 0.01,
     marginBottom: 33,
+    paddingTop: 92,
   },
   input: {
-    width: "92%",
-    height: 50,
-    backgroundColor: "#F6F6F6",
-    marginBottom: 16,
-    marginLeft: 16,
-    marginRight: 16,
-    padding: 16,
-    borderRadius: 8,
     borderWidth: 1,
     borderColor: "#E8E8E8",
-    justifyContent: "center",
-    fontSize: 16,
-  },
-  showPasswordText: {
-    fontSize: 16,
-    color: "#1B4371",
-    position: "absolute",
-    textAlign: "right",
-
-    right: 32,
-    bottom: 30,
-  },
-  registerBtn: {
-    width: "92%",
-    height: 51,
-    backgroundColor: "#FF6C00",
-    padding: 16,
-    borderRadius: 100,
-    margin: 16,
-  },
-  registerBtnText: {
-    fontSize: 16,
-    color: "#ffffff",
-    textAlign: "center",
-  },
-  disabledRegisterBtn: {
-    width: "92%",
-    height: 51,
+    borderRadius: 8,
     backgroundColor: "#F6F6F6",
-    padding: 16,
+    height: 50,
+    paddingLeft: 16,
+    marginBottom: 16,
+  },
+  btn: {
+    backgroundColor: "#FF6C00",
+    height: 51,
     borderRadius: 100,
-    margin: 16,
+    marginTop: 27,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  registerBtnTextDisabled: {
+  textBtn: {
+    color: "#fff",
+    fontFamily: "Roboto-Regular",
     fontSize: 16,
-    color: "#BDBDBD",
-    textAlign: "center",
+    lineHeight: 19,
   },
-  linkText: {
-    fontSize: 16,
+  textLogin: {
     color: "#1B4371",
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    marginTop: 16,
     textAlign: "center",
-    // marginTop: 16,
+  },
+  avatar: {
+    position: "absolute",
+    right: "50%",
+    top: 0,
+    transform: [{ translateX: 60 }, { translateY: -60 }],
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
+  avatarBtn: {
+    position: "absolute",
+    bottom: 19,
+    left: 105,
+    width: 25,
+    height: 25,
   },
 });
