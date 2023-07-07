0b3e50960d5ffa7e437529181d2259cfd3aa399e
OlgaTikhonchik <olga882011@gmail.com>
2023-07-07 18:45:22 +0300

    Update App.js

--- react-native-project/App.js
+++ react-native-project/App.js
@@ -1,18 +1,14 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
 import { useFonts } from "expo-font";
+import { useCallback } from "react";
 
-import RegistrationScreen from "./Screens/RegistrationScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import Home from "./Screens/Home";
-import CommentsScreen from "./Screens/CommentsScreen";
-import MapScreen from "./Screens/MapScreen";
-import PostsScreen from "./Screens/PostsScreen";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-
-const MainStack = createStackNavigator();
+import { View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Main from "./components/Main";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,44 +17,21 @@ export default function App() {
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <NavigationContainer>
-      <MainStack.Navigator
-        initialRouteName="Registration"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <MainStack.Screen name="Registration" component={RegistrationScreen} />
-        <MainStack.Screen name="Login" component={LoginScreen} />
-        <MainStack.Screen name="Home" component={Home} />
-        <MainStack.Screen name="Comments" component={CommentsScreen} />
-        <MainStack.Screen name="Map" component={MapScreen} />
-      </MainStack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <Provider store={store}>
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <Main />
+      </View>
+    </Provider>
   );
-
-  // return (
-  //   <View style={styles.container}>
-  //     {/* <RegistrationScreen /> */}
-  //     <LoginScreen />
-  //     {/* <PostsScreen /> */}
-  //     {/* <CommentsScreen /> */}
-  //     <StatusBar style="auto" />
-  //   </View>
-  // );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
